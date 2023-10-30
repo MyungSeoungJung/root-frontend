a386442553697a96eef6697a3c844707f8798098
sooyeol1002 <didtnduf2000@gmail.com>
2023-10-30 16:49:34 +0900

    테이블추가

--- src/modules/UserManagement/ReviewManagement/index.tsx
+++ src/modules/UserManagement/ReviewManagement/index.tsx
@@ -1,26 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { useReviews } from "./useReviews";
+import styled from "styled-components";
+
+const StyledTable = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+  th,
+  td {
+    border: 1px solid #ddd;
+    padding: 8px;
+    text-align: left;
+  }
+  th {
+    background-color: #f4f4f4;
+  }
+`;
 
 const ReviewsTable = () => {
   const { reviews, loading, error } = useReviews("brandName", "token");
+  const [sortKey, setSortKey] = useState("id");
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error loading reviews!</p>;
 
+  const sortedReviews = [...reviews].sort((a, b) => {
+    if (a[sortKey] < b[sortKey]) return -1;
+    if (a[sortKey] > b[sortKey]) return 1;
+    return 0;
+  });
+
+  const handleSort = (key) => {
+    setSortKey(key);
+  };
+
   return (
-    <table>
+    <StyledTable>
       <thead>
         <tr>
-          <th>ID</th>
-          <th>Brand Name</th>
-          <th>Product Number</th>
-          <th>Birth Date</th>
-          <th>Gender</th>
-          <th>Content</th>
+          <th onClick={() => handleSort("id")}>ID</th>
+          <th onClick={() => handleSort("brandName")}>Brand Name</th>
+          <th onClick={() => handleSort("productNumber")}>Product Number</th>
+          <th onClick={() => handleSort("birthDate")}>Birth Date</th>
+          <th onClick={() => handleSort("gender")}>Gender</th>
+          <th onClick={() => handleSort("content")}>Content</th>
         </tr>
       </thead>
       <tbody>
-        {reviews.map((review) => (
+        {sortedReviews.map((review) => (
           <tr key={review.id}>
             <td>{review.id}</td>
             <td>{review.brandName}</td>
@@ -31,7 +57,7 @@ const ReviewsTable = () => {
           </tr>
         ))}
       </tbody>
-    </table>
+    </StyledTable>
   );
 };
 
